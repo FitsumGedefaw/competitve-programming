9813edffd6d6037d066788f075cd573baef49aa4
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-03-28 12:46:19 +0100

    Create SortColors.java

---
+++ SortColors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+            for(int i = n - 2; i >= 0; i--)                       
+                {
+                    for(int j = 0; j <= i; j++)
+                        {
+                            if(nums[j] > nums[j+1])
+                                {
+                                    int temp = nums[j];
+                                    nums[j] = nums[j+1];
+                                    nums[j+1] = temp;
+                                }
+                        }
+                }
+    }
+}
