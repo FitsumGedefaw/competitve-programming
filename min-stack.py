013fe7e15424eddcb68d40f2f158792096355476
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-08-08 03:14:41 -0700

    changed the array implementation to deque

--- min-stack.py
+++ min-stack.py
@@ -1,8 +1,8 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.minStack = []
+        self.stack = deque()
+        self.minStack = deque()
     def push(self, val: int) -> None:
         self.stack.append(val)
         minElement = min(val, self.minStack[-1] if self.minStack else val)
