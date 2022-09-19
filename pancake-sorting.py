e9e2312e6dfb0572b7ba66263bbddcb1bd7273c0
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-09-19 22:45:52 +0300

    Update pancake-sorting.py

--- pancake-sorting.py
+++ pancake-sorting.py
@@ -1,19 +1,19 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        def flip(r):
-            l = 0
-            while l < r:
-                arr[l], arr[r] = arr[r], arr[l]
-                l += 1
-                r -= 1
-        result = []
-        N = len(arr)
-        for i in range(N, 0, -1):
-            subArr = arr[:i]
-            maxIndex = arr.index(max(subArr))
-            if maxIndex != i - 1:
-                flip(maxIndex)
-                flip(i-1)
-                result.append(maxIndex+1)
-                result.append(i)
-        return result
+        res = []
+        val = len(arr)
+        for i in range(len(arr) - 1, -1, -1):
+            valInd = arr.index(val)
+            if valInd != i:
+                l, r = 0, valInd
+                while l < r:
+                    arr[l], arr[r] = arr[r], arr[l]
+                    l, r = l + 1, r - 1
+                res.append(valInd + 1)
+                left, right = 0, i
+                while left < right:
+                    arr[left], arr[right] = arr[right], arr[left]
+                    left, right = left + 1, right - 1
+                res.append(i + 1)
+            val -= 1
+        return res
