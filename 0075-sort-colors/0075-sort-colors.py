7b886b7d23e743a77c65dd2830730864d8393898
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-10-15 23:36:15 +0300

    Time: 70 ms (24.62%), Space: 14 MB (15.34%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i, l, r = 0, 0, len(nums)-1
+        while i <= r:
+            if nums[i] == 0:
+                nums[l], nums[i] = nums[i], nums[l]
+                l += 1
+            elif nums[i] == 2:
+                nums[r], nums[i] = nums[i], nums[r]
+                r, i = r-1, l-1
+            i += 1
+        
