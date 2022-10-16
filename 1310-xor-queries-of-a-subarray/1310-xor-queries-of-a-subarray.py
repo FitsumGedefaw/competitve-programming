30fff227ebf6d2062625c4b4a6f42656458e2208
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-10-16 16:18:55 +0300

    Time: 1025 ms (16.84%), Space: 28.7 MB (82.18%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        res = []
+        for i in range(1,len(arr)):
+            arr[i] = arr[i] ^ arr[i-1]
+        for l,r in queries:
+            if l == 0:
+                res.append(arr[r])
+            else:
+                res.append(arr[r] ^ arr[l-1])
+        return res
+    
+            
