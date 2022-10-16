9755d03334edbd8c5a44a15b459a428d384daacb
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-10-16 11:25:43 +0300

    Time: 1241 ms (5.02%), Space: 16.6 MB (65.67%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,29 +1,14 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        res = 0
-        k = 1 # max zeroes we can delete
-        l = 0
-        
-        for r in range(len(nums)):
-            # if we encounter a zero, decrement
-            # the num zeroes we can now delete
+        l = r = maxlen = 0
+        k = 1
+        while r < len(nums):
             if nums[r] == 0:
                 k -= 1
-
-            # if we've seen more than one zero
-            # move the left pointer until we
-            # have discarded the first zero
-            # we have encountered hence making the
-            # sliding window (l..r) valid again
             while k < 0 and l <= r:
                 if nums[l] == 0:
                     k += 1
                 l += 1
-            
-            # make sure you're not adding +1
-            # to the sliding window because we
-            # want to also account for the deleted
-            # zero, so don't index correct here
-            res = max(res, r-l)
-            
-        return res
+            maxlen = max(maxlen, r-l)
+            r += 1
+        return maxlen
