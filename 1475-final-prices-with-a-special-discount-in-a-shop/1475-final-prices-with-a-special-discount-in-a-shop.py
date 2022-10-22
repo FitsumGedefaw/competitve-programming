bcd75968734681d46f9b2ef1c5a44eccb56ff6ef
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-10-22 18:55:52 +0300

    Time: 123 ms (16.51%), Space: 14 MB (61.77%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -2,10 +2,10 @@ class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         st = deque()
         res = list(prices)
-        for i in range(len(prices)):
-            while st and prices[i] <= prices[st[-1]]:
+        for i,v in enumerate(prices):
+            while st and v <= prices[st[-1]]:
                 j = st.pop()
-                res[j] = prices[j] - prices[i]
+                res[j] = prices[j] - v
             st.append(i)
         return res
         
