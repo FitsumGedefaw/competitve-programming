f6ff27baf771914927559814889d8f1050873749
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-10-25 19:25:18 +0300

    Time: 73 ms (48.24%), Space: 13.8 MB (71.13%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,17 +1,15 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        stus = deque(students)
-        sans = deque(sandwiches)
-        while stus:
-            if stus[0] == sans[0]:
-                stus.popleft()
-                sans.popleft()
+        while students:
+            if students[0] == sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
             else:
-                k=0
-                while stus[0] != sans[0]:
+                k = 0
+                while students[0] != sandwiches[0]:
                     k += 1
-                    stus.append(stus.popleft())
-                    if k == len(stus):
+                    students.append(students.pop(0))
+                    if k == len(students):
                         return k
         return 0
                     
