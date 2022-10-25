85a2281947ffe8a8809657f383cb8961ef637054
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-10-25 12:15:01 +0300

    Time: 80 ms (36.35%), Space: 14 MB (27.55%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,20 +1,14 @@
 class Solution:
-    def removeOuterParentheses(self, s: str) -> str:
+    def removeOuterParentheses(self, S: str) -> str:
+        res = basket = ""
         st = deque()
-        res = temp = ""
-        nlb = nrb = 0
-        for i in range(len(s)):
-            if s[i] == "(":
-                nlb += 1
-                st.append(s[i])
+        for i in range(len(S)):
+            if S[i] == "(":
+                st.append(S[i])
             else:
-                nrb += 1
-                if nlb == nrb:
-                    while len(st) > 1:
-                        temp = st.pop() + temp
-                    st.pop()
-                    res, temp, nlb, nrb = res+temp, "", 0, 0
-                else:
-                    st.append(s[i])
+                st.pop()
+            basket += S[i]
+            if len(st) == 0:
+                res += basket[1: -1]
+                basket = ""
         return res
-                    
