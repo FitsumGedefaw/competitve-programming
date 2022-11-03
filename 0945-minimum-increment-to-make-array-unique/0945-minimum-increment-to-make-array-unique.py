4fc8a27c8c5cceb163b161502c4956843b5f8b56
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-11-03 20:55:29 +0300

    Time: 2403 ms (26.68%), Space: 28.2 MB (68.95%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        ans = 0
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                ans += nums[i-1] + 1 - nums[i]
+                nums[i] = nums[i-1] + 1
+        return ans
+        
+        
+        
