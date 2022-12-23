6140127fb15f8fe9a90888d74e9363800ad9d1ba
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-12-23 17:46:39 +0300

    Time: 81 ms (74.76%), Space: 13.9 MB (16.14%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,15 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         num = str(x)
-        left, right = 0, len(num)-1
         
-        while left < right:
-            if num[left] != num[right]:
-                return False
-            
-            left, right = left+1, right-1
-            
-        return True
+        return num == num[::-1]
         
         
         
