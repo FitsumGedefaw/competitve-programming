0825e16c3b588b869606a9244ddb71ad43aeb5bb
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2022-12-23 18:34:55 +0300

    Time: 84 ms (73.36%), Space: 13.9 MB (59.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        num = str(x)
+        reversedNum = 0
         
-        return num == num[::-1]
+        if x < 0:
+            return False
+    
+        originalNum = x
         
+        while x != 0:
+            lastDigit = x % 10
+            
+            reversedNum = (reversedNum * 10) + lastDigit
+            
+            x = x // 10
+        
+        return originalNum == reversedNum 
         
         
