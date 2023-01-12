ef99b1e0b67fdd59f468a08472b583ce17d96532
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-01-12 11:35:45 +0300

    Time: 36 ms (87.96%), Space: 13.9 MB (42.59%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -2,11 +2,11 @@ class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         arr = [i for i in range(1, n+1)]
         
-        i, j = 0, k-1
+        start, end = 0, k-1
         
         while len(arr) > 1:
-            arr.pop(j)
-            i = j % len(arr)
-            j = (i + k - 1) % len(arr)
+            arr.pop(end)
+            start = end % len(arr)
+            end = (start + k - 1) % len(arr)
             
         return arr[0]
