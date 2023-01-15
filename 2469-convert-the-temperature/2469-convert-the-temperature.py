4ab1598b439ac436635a24f63a2d0950a3340c06
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-01-15 12:02:32 +0300

    Time: 24 ms (97.84%), Space: 13.8 MB (94.39%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,10 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        result = []
+
+        Kevin = celsius + 273.15
+        Fahrenheit = celsius *1.80 +32.00
+        result.append (Kevin)
+        result.append (Fahrenheit)
+
+        return result
