4b03a56d03545b9321c905d16d63a616bd5844bb
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-01-21 07:14:43 +0300

    Time: 40 ms (55.63%), Space: 13.9 MB (51.23%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -2,13 +2,12 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = Counter(nums)
         
-        #for n in nums:
-        #    count[n] += 1
-            
-        res = 0
+        numOfGoodPairs = 0
         
-        for i in nums:
-            if count[i] > 1:
-                res += count[i] - 1
-                count[i] -= 1
-        return res
+        for num in count:
+            n = count[num]
+            
+            numOfGoodPairs += (n * (n-1)//2)
+            
+        return numOfGoodPairs
+            
