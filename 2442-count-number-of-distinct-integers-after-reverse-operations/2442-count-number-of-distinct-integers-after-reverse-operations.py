51333b581ce966fe3bd9325492a09f6d19143b18
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-01-24 10:05:15 +0300

    Time: 1038 ms (38.80%), Space: 39.2 MB (84.57%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
@@ -2,24 +2,15 @@ class Solution:
     def reverseInt(self, num):
         numStr = str(num)
         
-        temp = [0] * len(numStr)
-        
-        for j in range(len(numStr)-1, -1, -1):
-            temp[len(numStr)-1-j] = numStr[j]
-            
-        while temp[0] == "0":
-            temp.pop(0)
-        
-        return [numStr, "".join(temp)]
+        return int(numStr[::-1])
         
         
     def countDistinctIntegers(self, nums: List[int]) -> int:
         distinictNums = set()
         
         for num in nums:
-            numStr, numStrRev = self.reverseInt(num)
-            distinictNums.add(numStr)
-            distinictNums.add(numStrRev)
+            distinictNums.add(num)
+            distinictNums.add(self.reverseInt(num))
             
         return len(distinictNums)
             
