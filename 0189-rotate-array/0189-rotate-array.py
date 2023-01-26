10470d26145f8d03f0e3a638e02cb1b0dd99c222
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-01-26 10:35:49 +0300

    Time: 236 ms (64.86%), Space: 25.5 MB (6.91%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,12 +1,24 @@
 class Solution:
+    
+    #Time Complexity: O(n)
+    #Space Complexity: O(1)
+    
+    def reverse(self, l, r, nums):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+    
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
-        while k > 0:
-            v = nums.pop()
-            nums.insert(0,v)
-            k -= 1
+        
+        self.reverse(0, len(nums)-1, nums)
+        self.reverse(0, k-1, nums)
+        self.reverse(k, len(nums)-1, nums)
+        
+        
+        
         
         
