228909d283f6c65ed6a0674320ab80b7279301e8
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-01-26 07:31:06 +0300

    Time: 42 ms (60.02%), Space: 13.9 MB (80.30%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,14 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m
-        j = 0
+        i = m-1
+        j = n-1
         
-        while j < n:
-            nums1[i] = nums2[j]
-            i, j = i+1, j+1
+        for insertIdx in range(m+n-1, -1, -1):
+            if i > -1 and j > -1:
+                
+                if nums1[i] > nums2[j]:
+                    nums1[insertIdx] = nums1[i]
+                    i -= 1
+                
+                else:
+                    nums1[insertIdx] = nums2[j]
+                    j -= 1
+                    
+            elif i > -1:
+                nums1[insertIdx] = nums1[i]
+                i -= 1
+                
+            else:
+                nums1[insertIdx] = nums2[j]
+                j -= 1
+                
+                    
             
-        nums1.sort()
+        
+        
+    
         
             
         
