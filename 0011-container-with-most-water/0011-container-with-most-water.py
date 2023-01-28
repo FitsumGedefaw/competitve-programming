4fc2103fe74c827bdc343e49fe43b3fbd9a2b430
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-01-28 09:25:13 +0300

    Time: 2435 ms (5.01%), Space: 26.1 MB (96.48%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        ma = 0
         l, r = 0, len(height)-1
+        
+        maxAmount = 0
+        
         while l < r:
-            if height[l] <= height[r]:
-                a = height[l] * (r-l)
-                ma = max(ma, a)
-                l += 1
-            else:
-                a = height[r] * (r-l)
-                ma = max(ma, a)
+            amount = (r-l) * min(height[l], height[r])
+            
+            maxAmount = max(maxAmount, amount)
+            
+            if height[r] <= height[l]:
                 r -= 1
-        return ma
                 
+            else:
+                l += 1
+                
+        return maxAmount
             
         
