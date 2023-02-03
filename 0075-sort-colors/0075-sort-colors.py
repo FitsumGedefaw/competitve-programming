1d9c79f4fccd56f7a1dc85d90e6f02e148c4e389
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-02-03 14:59:16 +0300

    Time: 39 ms (53.88%), Space: 13.9 MB (56.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,13 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i, l, r = 0, 0, len(nums)-1
+        l, r, i = 0, len(nums)-1, 0
+        
         while i <= r:
             if nums[i] == 0:
                 nums[l], nums[i] = nums[i], nums[l]
-                l += 1
+                l, i = l+1, i+1
+                
             elif nums[i] == 2:
                 nums[r], nums[i] = nums[i], nums[r]
-                r, i = r-1, l-1
-            i += 1
-        
+                r -= 1
+                
+            else:
+                i += 1
+                
+            
