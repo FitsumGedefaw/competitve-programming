2524b1bdb7372891335e64bd49825e91c523d949
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-02-24 15:48:41 +0300

    Time: 1429 ms (62.96%), Space: 34.3 MB (6.08%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,10 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans = [0] * len(temperatures)
-        st = deque()
-        for i, v in enumerate(temperatures):
-            while st and st[-1][1] < v:
-                ans[st[-1][0]] = i - st[-1][0]
-                st.pop()
-            st.append([i,v])
-        return ans
+        st = []
+        nexGreaIdx = {}
+        
+        for idx, val in enumerate(temperatures):
+            while st and temperatures[st[-1]] < val:
+                nexGreaIdx[st.pop()] = idx
+                
+            st.append(idx)
+               
+        for i in range(len(temperatures)):
+            temperatures[i] = nexGreaIdx[i] - i if i in nexGreaIdx else 0
+        
+        return temperatures
+        
