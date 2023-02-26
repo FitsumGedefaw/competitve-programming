e169cd573b842d2b7075dedcda835c9ba95bae5b
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-02-26 23:02:22 +0300

    Time: 281 ms (35.22%), Space: 27.1 MB (84.23%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,20 +1,21 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        l = r = 0
-        minlen = len(nums)
-        windowSum = 0
+        minlen = float('inf')
+        winSum = 0
+        
+        l = 0
+        r = -1
+        
         while r < len(nums):
-            windowSum += nums[r]
-            while windowSum >= target:
-                minlen = min(minlen, r-l+1)
-                l, windowSum = l+1, windowSum - nums[l]
-            r += 1
-        if minlen == len(nums):
-            totalSum = 0
-            for num in nums:
-                totalSum += num
-            if totalSum < target:
-                return 0
-        return minlen
-            
-            
+            if winSum >= target:
+                minlen = min(minlen, r-l+1 )
+                winSum -= nums[l]
+                l += 1
+                
+            else:
+                r += 1
+                if r < len(nums):
+                    winSum += nums[r]
+                    
+        return minlen if minlen != float('inf') else 0
+        
