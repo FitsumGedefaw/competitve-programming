07ca7564a96eee27ba4e3d2ccccf1a73ba3e82d7
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-02-28 10:57:23 +0300

    Time: 80 ms (76.78%), Space: 13.6 MB (99.89%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -2,6 +2,8 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n == 1:
             return True
+        elif n % 3 != 0:
+            return False
         elif n < 1:
             return False
         
