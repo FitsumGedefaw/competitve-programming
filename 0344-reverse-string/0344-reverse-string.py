8be7b7e1690df0c2aad34c6356939c553860ba3b
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-03-02 09:57:43 +0300

    Time: 192 ms (92.74%), Space: 43.2 MB (5.46%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,11 +1,15 @@
 class Solution:
+    def revHelper(self, left, right, s):
+        if left >= right:
+            return 
+        
+        s[left], s[right] = s[right], s[left]
+        self.revHelper(left+1, right-1, s)
+            
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        l, r = 0, len(s)-1
+        self.revHelper(0, len(s)-1, s)
         
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l, r = l + 1, r - 1
         
