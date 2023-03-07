4abb4865a3dfd758874a5183c844220a561af839
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-03-07 15:14:48 +0300

    Time: 139 ms (55.07%), Space: 16.9 MB (89.00%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -5,28 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def insertHelper(self, root, val):
-        # if root == None:
-        #     root = ListNode(val)
-        #     return
+#     def insertHelper(self, root, val):
+#         # if root == None:
+#         #     root = ListNode(val)
+#         #     return
             
-        if val < root.val:
-            if root.left == None:
-                root.left = TreeNode(val)
-                return
-            else:
-                self.insertHelper(root.left, val)
+#         if val < root.val:
+#             if root.left == None:
+#                 root.left = TreeNode(val)
+#                 return
+#             else:
+#                 self.insertHelper(root.left, val)
         
-        else:
-            if root.right == None:
-                root.right = TreeNode(val)
-                return
-            else:
-                self.insertHelper(root.right, val)
+#         else:
+#             if root.right == None:
+#                 root.right = TreeNode(val)
+#                 return
+#             else:
+#                 self.insertHelper(root.right, val)
     
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        curr = root
         if not root:
             return TreeNode(val)
-        self.insertHelper(curr, val)
-        return curr
+        
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+            
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+            
+        return root
+        
+        # return curr
