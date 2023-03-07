757021e9e591b2bfb5f65b59f320177e00f53c9b
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-03-07 06:02:29 +0300

    Time: 59 ms (31.07%), Space: 13.8 MB (46.06%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low, high = 0, x
+        
+        while low <= high:
+            mid = (low+high)//2
+            
+            if mid**2 == x:
+                return mid
+            
+            elif mid ** 2 > x:
+                high = mid-1
+              
+            else:
+                low = mid+1 
+                    
+        return low-1 
+                
+        
+    
+        
+        
+        
