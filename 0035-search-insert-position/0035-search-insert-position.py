2c35e800bcacbff47a8e18dac0bbec6f8d67d708
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-03-12 23:30:53 +0300

    Time: 56 ms (32.97%), Space: 14.6 MB (71.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,20 +1,4 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        low, high = 0, len(nums)-1
-        
-        while low <= high:
-            mid = (low+high)//2
-            
-            if nums[mid] < target:
-                low = mid+1
-            
-            elif nums[mid] > target:
-                high = mid-1
-                
-            else:
-                return mid
-            
-        return mid if nums[mid] > target else mid+1
-        
-        
+        return bisect_left(nums, target)
         
