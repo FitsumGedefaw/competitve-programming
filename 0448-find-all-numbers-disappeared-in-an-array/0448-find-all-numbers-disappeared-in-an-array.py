e78bd76f8cf7ab67e84a4fe880d4f986392cfffc
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-03-28 23:14:24 +0300

    Time: 413 ms (11.36%), Space: 22.2 MB (67.91%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,6 +1,15 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        nums = Counter(nums)
-        return [num for num in range(1, n+1) if num not in nums]
+        i = 0
+        
+        while i < len(nums):
+            corrPos = nums[i] - 1
+            
+            if i == corrPos or nums[i] == nums[corrPos]:
+                i += 1
+            else:
+                nums[i], nums[corrPos] = nums[corrPos], nums[i]
+                
+        return [i+1 for i in range(len(nums)) if i != nums[i]-1]        
+            
         
