ac9abd7e0e4079963eec7e3c4e68c9858a3bf815
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-03-28 19:44:57 +0300

    Time: 159 ms (24.73%), Space: 15.3 MB (28.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,27 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        existingNums = {num for num in nums}
+        missingNum = -1
+        nums.append(-1)
         
-        #missingNum = -1
+        i = 0
+        while i < len(nums):
+            while nums[i] != -1 and i != nums[i]:
+                x = nums[i]
+                nums[i], nums[x] = nums[x], nums[i]
+            
+            if nums[i] == -1:
+                missingNum = i
+            
+            i += 1
+            
+        return missingNum
         
-        for num in range(0, len(nums)+1):
-            if num not in existingNums:
-                return num
-                
         
+            
+            
+        
+        
+                    
+             
             
         
