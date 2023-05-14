270ae185b1fcda694a693e226559a5cf137abbd5
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-05-14 22:52:00 +0300

    Create Detect cycle in an undirected graph.py

---
+++ Geeks_for_Geeks/Detect cycle in an undirected graph.py
@@ -0,0 +1,45 @@
+from typing import List
+class Solution:
+    #Function to detect cycle in an undirected graph.
+	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+		#Code here
+		visited = set()
+		
+		def dfs(node, par):
+		    visited.add(node)
+		    
+		    for neigh in adj[node]:
+		        if neigh != par:
+    		        if neigh in visited:
+    		            return 1
+    		        if dfs(neigh, node):
+    		            return 1
+    		        
+    	for node in range(V):
+    	    if node not in visited:
+    	        if dfs(node, -1):
+    	            return 1
+    	    
+    	return 0
+	    	
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+
+	T=int(input())
+	for i in range(T):
+		V, E = map(int, input().split())
+		adj = [[] for i in range(V)]
+		for _ in range(E):
+			u, v = map(int, input().split())
+			adj[u].append(v)
+			adj[v].append(u)
+		obj = Solution()
+		ans = obj.isCycle(V, adj)
+		if(ans):
+			print("1")
+		else:
+			print("0")
+
+# } Driver Code Ends
