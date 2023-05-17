b7b70f61a7b9fb4b066f9c11933439c032641ea4
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-05-17 18:25:08 +0300

    Time: 2395 ms (16.41%), Space: 106.5 MB (64.84%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,36 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        rep = [i for i in range(n)]
+        size = [1 for i in range(n)]
+        
+        def findRep(node):
+            if node == rep[node]:
+                return node
+            
+            rep[node] = findRep(rep[node])
+            return rep[node]
+        
+        def union(x, y):
+            xrep = findRep(x)
+            yrep = findRep(y)
+            
+            if xrep != yrep:
+                if size[xrep] <= size[yrep]:
+                    rep[xrep] = yrep
+                    size[yrep] += size[xrep]
+                    
+                else:
+                    rep[yrep] = xrep
+                    size[xrep] += size[yrep]
+                    
+        def connected(x, y):
+            return findRep(x) == findRep(y)
+        
+        for src, dst in edges:
+            print()
+            print(src, dst)
+            union(src, dst)
+            print()
+        return connected(source, destination)
+            
+        
