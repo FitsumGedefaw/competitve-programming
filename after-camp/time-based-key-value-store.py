391e114e7c79651c2eaf24b5b50b887cfdc5ac5f
GFitsum <91980891+FitsumGedefaw@users.noreply.github.com>
2023-11-13 14:30:40 +0300

    Added solution for Time Based Key-Value Store

---
+++ after-camp/time-based-key-value-store.py
@@ -0,0 +1,35 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dict = defaultdict(list)
+        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dict[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        #largest prev_timestamp that is smaller or equal to timestamp
+        if key not in self.dict:
+            return ""
+        left, right = 0, len(self.dict[key]) - 1
+        while left < right:
+            mid = ceil(left + (right - left) / 2)
+         
+            if self.dict[key][mid][0] <= timestamp:
+                left = mid 
+            else:
+                right = mid - 1
+        
+        if self.dict[key][left][0] > timestamp:
+            return "" 
+
+        return self.dict[key][left][1]
+
+        
+
+
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
